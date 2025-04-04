80c4538bbc8082cf18575e6d6ffa147da8ff1a15
Alba Cardona <alba.cardonajimenez@gmail.com>
2025-04-04 14:18:26 +0200

    chore: test replacing BrowserRouter with HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Navbar } from './app/components/Navbar';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import { HomePage } from './app/pages/HomePage';
 import { AboutMe } from './app/pages/AboutMe';
 import { Projects } from './app/pages/Projects';
@@ -8,7 +8,7 @@ import { PersonalProjects } from './app/pages/PersonalProjects';
 
 export const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <main className="h-screen bg-bg-primary text-fg-primary overflow-auto">
         <Navbar />
         <Routes>
@@ -19,6 +19,6 @@ export const App = () => {
           <Route path="/personal-projects" element={<PersonalProjects />} />
         </Routes>
       </main>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
