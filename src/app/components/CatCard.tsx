a585131900485f8bc30a11c3bdefdddd367a4230
Alba Cardona <alba.cardonajimenez@gmail.com>
2025-04-04 14:03:39 +0200

    feat: add mobile menu and adapt navbar

--- src/app/components/CatCard.tsx
+++ src/app/components/CatCard.tsx
@@ -17,7 +17,7 @@ export const CatCard = ({ cat, isFlipped, isFlipping, onClickCard }: Props) => {
       disabled={isFlipping}
       aria-label="cat-card"
       className={clsx({
-        'relative size-20 md:size-36 flex flex-col items-center justify-center p-4 bg-bg-primary rounded-lg shadow-md [perspective:1000px] transition-all duration-500 [transform-style:preserve-3d]': true,
+        'relative size-20 sm:size-36 flex flex-col items-center justify-center p-4 bg-bg-primary rounded-lg shadow-md [perspective:1000px] transition-all duration-500 [transform-style:preserve-3d]': true,
         '[transform:rotateY(180deg)]': isFlipped,
       })}
     >
@@ -25,11 +25,11 @@ export const CatCard = ({ cat, isFlipped, isFlipping, onClickCard }: Props) => {
         <img
           src={cat.image}
           alt={cat.name}
-          className="size-16 md:size-32 object-cover rounded-lg [transform:rotateY(180deg)]"
+          className="size-16 sm:size-32 object-cover rounded-lg [transform:rotateY(180deg)]"
         />
       </div>
-      <div className="absolute size-16 md:size-32 flex items-center justify-center rounded-lg bg-bg-primary [backface-visibility:hidden]">
-        <img src={logo} alt="logo" className="size-8 md:size-16 object-cover rounded-lg" />
+      <div className="absolute size-16 sm:size-32 flex items-center justify-center rounded-lg bg-bg-primary [backface-visibility:hidden]">
+        <img src={logo} alt="logo" className="size-8 sm:size-16 object-cover rounded-lg" />
       </div>
     </button>
   );
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,8 +2,52 @@ import { Link } from 'react-router-dom';
 import { LanguageSelect } from './LanguageSelect';
 import { ThemeToggle } from './ThemeToggle';
 import logo from '@/assets/img/ACJ.png';
+import { Menu01 } from '@untitled-ui/icons-react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 export const Navbar = () => {
+  const renderMobileMenu = () => {
+    return (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <button type="button" className="cursor-pointer text-sm">
+            <Menu01 onClick={renderMobileMenu} />
+          </button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-fit">
+          <DropdownMenuItem>
+            <Link to="/projects" className="text-sm">
+              My projects
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link to="/about" className="text-sm">
+              About me
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link to="/cats" className="text-sm">
+              My cats
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link to="/personal-projects" className="text-sm">
+              Personal projects
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem className="justify-between">
+            <LanguageSelect />
+            <ThemeToggle />
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    );
+  };
   return (
     <nav className="h-16 w-full p-4 flex items-center justify-between bg-transparent text-fg-primary shadow-md">
       <ul className="flex items-center gap-4">
@@ -12,28 +56,28 @@ export const Navbar = () => {
             <img src={logo} alt="Logo" className="size-10" />
           </Link>
         </li>
-        <li>
+        <li className="hidden sm:block">
           <Link to="/projects" className="text-sm">
             My projects
           </Link>
         </li>
-        <li>
+        <li className="hidden sm:block">
           <Link to="/about" className="text-sm">
             About me
           </Link>
         </li>
-        <li>
+        <li className="hidden sm:block">
           <Link to="/cats" className="text-sm">
             My cats
           </Link>
         </li>
-        <li>
+        <li className="hidden sm:block">
           <Link to="/personal-projects" className="text-sm">
             Personal projects
           </Link>
         </li>
       </ul>
-      <ul className="flex items-center gap-4">
+      <ul className="hidden sm:flex items-center gap-4">
         <li>
           <LanguageSelect />
         </li>
@@ -41,6 +85,8 @@ export const Navbar = () => {
           <ThemeToggle />
         </li>
       </ul>
+
+      <div className="sm:hidden">{renderMobileMenu()}</div>
     </nav>
   );
 };
