b2dd28b75dbdcb168d77f5c317e58dfb5f86b755
Alba Cardona <alba.cardonajimenez@gmail.com>
2025-04-11 15:59:06 +0200

    chore: remove i18next

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Navbar } from './app/components/Navbar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { HomePage } from './app/pages/HomePage';
 
@@ -6,7 +5,6 @@ export const App = () => {
   return (
     <BrowserRouter>
       <main className="h-screen bg-bg-primary text-fg-primary overflow-auto">
-        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
